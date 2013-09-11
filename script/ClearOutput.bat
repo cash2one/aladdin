70e4751635bda4a4a07e446404d1c7324e64f08d
xfork <liuheng0204@gmail.com>
2013-09-11 18:24:34 +0800

    a

---
+++ script/ClearOutput.bat
@@ -0,0 +1,7 @@
+rd /Q /S D:\autopack\output\aladdin\installers\bind
+rd /Q /S D:\autopack\output\aladdin\installers\bind1
+rd /Q /S D:\autopack\output\aladdin\installers\bind2
+rd /Q /S D:\autopack\output\aladdin\installers\src
+rd /Q /S D:\autopack\output\aladdin\installers\unbind
+rd /Q /S D:\autopack\output\aladdin\installers\update
+xcopy /Y /E /S /I D:\autopack\output\backup\installers D:\autopack\output\aladdin\installers
