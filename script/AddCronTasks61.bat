19a430d508afd2e11bfcf91b98a1fcd289a8764c
xfork <liuheng0204@gmail.com>
2013-09-17 16:38:10 +0800

    update

--- script/AddCronTasks61.bat
+++ script/AddCronTasks61.bat
@@ -1,4 +1,4 @@
-SCHTASKS /RU administrator /RP 1qaz2wsx /Create /TN "update_all_xml_1" /SC DAILY /ST 00:00:00 /TR "python D:\autopack\script\fetchxml.py -fdtb"
+SCHTASKS /RU administrator /RP 1qaz2wsx /Create /TN "update_all_xml_1" /SC DAILY /ST 00:01:00 /TR "python D:\autopack\script\fetchxml.py -fdtb"
 
 SCHTASKS /RU administrator /RP 1qaz2wsx /Create /TN "update_daily_xml_1" /SC DAILY /ST 08:00:00 /TR "python D:\autopack\script\fetchxml.py -dtb"
 
