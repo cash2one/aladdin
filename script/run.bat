fd857cc44fd18001d0dbe16bc7f1d13677fbf855
xfork <liuheng0204@gmail.com>
2013-10-22 21:17:09 +0800

    update

--- script/run.bat
+++ script/run.bat
@@ -1,3 +1,19 @@
 aladdin.py -brt baidusd -s 15726 -g bind1
+aladdin.py -bcRt baidusd -g bind1
+aladdin.py -bcRt baidusd -p ..\info\95_aladin_id.txt -P ..\info\ignored.txt -g bind1
+
+aladdin.py -brt baidusd -s 15726 -g bind2
+aladdin.py -bcRt baidusd -g bind2
+aladdin.py -bcRt baidusd -p ..\info\95_aladin_id.txt -P ..\info\ignored.txt -g bind2
+
+aladdin.py -brt baidusd -s 15726 -g bind3
 aladdin.py -bcRt baidusd -g bind3
 aladdin.py -bcRt baidusd -p ..\info\95_aladin_id.txt -P ..\info\ignored.txt -g bind3
+
+aladdin.py -brt baidusd -s 15726 -g bind4
+aladdin.py -bcRt baidusd -g bind4
+aladdin.py -bcRt baidusd -p ..\info\95_aladin_id.txt -P ..\info\ignored.txt -g bind4
+
+aladdin.py -brt baidusd -s 15726 -g bind5
+aladdin.py -bcRt baidusd -g bind5
+aladdin.py -bcRt baidusd -p ..\info\95_aladin_id.txt -P ..\info\ignored.txt -g bind5
