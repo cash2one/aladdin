421a4c1734e495b86988ad9054e9c4204562ac6e
xfork <liuheng0204@gmail.com>
2013-10-22 21:21:07 +0800

    update

--- script/run.bat
+++ script/run.bat
@@ -1,3 +1,5 @@
+fetchxml.py -fdt
+
 aladdin.py -brt baidusd -s 15726 -g bind1
 aladdin.py -bcRt baidusd -g bind1
 aladdin.py -bcRt baidusd -p ..\info\95_aladin_id.txt -P ..\info\ignored.txt -g bind1
