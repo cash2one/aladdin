70c16c94580ecf0416698e4474e0a64bd8276e27
xfork <liuheng0204@gmail.com>
2013-11-13 12:15:03 +0800

    update

--- script/run.bat
+++ script/run.bat
@@ -66,4 +66,68 @@ aladdin.py -bcrRt baidusd -s 15726 -g bind15
 aladdin.py -bcRt baidusd -g bind15
 aladdin.py -bcRt baidusd -p ..\info\95_aladin_id.txt -P ..\info\ignored.txt -g bind15
 
+aladdin.py -bcrRt baidusd -s 15726 -g bind16
+aladdin.py -bcRt baidusd -g bind16
+aladdin.py -bcRt baidusd -p ..\info\95_aladin_id.txt -P ..\info\ignored.txt -g bind16
+
+aladdin.py -bcrRt baidusd -s 15726 -g bind17
+aladdin.py -bcRt baidusd -g bind17
+aladdin.py -bcRt baidusd -p ..\info\95_aladin_id.txt -P ..\info\ignored.txt -g bind17
+
+aladdin.py -bcrRt baidusd -s 15726 -g bind18
+aladdin.py -bcRt baidusd -g bind18
+aladdin.py -bcRt baidusd -p ..\info\95_aladin_id.txt -P ..\info\ignored.txt -g bind18
+
+aladdin.py -bcrRt baidusd -s 15726 -g bind19
+aladdin.py -bcRt baidusd -g bind19
+aladdin.py -bcRt baidusd -p ..\info\95_aladin_id.txt -P ..\info\ignored.txt -g bind19
+
+aladdin.py -bcrRt baidusd -s 15726 -g bind20
+aladdin.py -bcRt baidusd -g bind20
+aladdin.py -bcRt baidusd -p ..\info\95_aladin_id.txt -P ..\info\ignored.txt -g bind20
+
+aladdin.py -bcrRt baidusd -s 15726 -g bind21
+aladdin.py -bcRt baidusd -g bind21
+aladdin.py -bcRt baidusd -p ..\info\95_aladin_id.txt -P ..\info\ignored.txt -g bind21
+
+aladdin.py -bcrRt baidusd -s 15726 -g bind22
+aladdin.py -bcRt baidusd -g bind22
+aladdin.py -bcRt baidusd -p ..\info\95_aladin_id.txt -P ..\info\ignored.txt -g bind22
+
+aladdin.py -bcrRt baidusd -s 15726 -g bind23
+aladdin.py -bcRt baidusd -g bind23
+aladdin.py -bcRt baidusd -p ..\info\95_aladin_id.txt -P ..\info\ignored.txt -g bind23
+
+aladdin.py -bcrRt baidusd -s 15726 -g bind24
+aladdin.py -bcRt baidusd -g bind24
+aladdin.py -bcRt baidusd -p ..\info\95_aladin_id.txt -P ..\info\ignored.txt -g bind24
+
+aladdin.py -bcrRt baidusd -s 15726 -g bind25
+aladdin.py -bcRt baidusd -g bind25
+aladdin.py -bcRt baidusd -p ..\info\95_aladin_id.txt -P ..\info\ignored.txt -g bind25
+
+aladdin.py -bcrRt baidusd -s 15726 -g bind26
+aladdin.py -bcRt baidusd -g bind26
+aladdin.py -bcRt baidusd -p ..\info\95_aladin_id.txt -P ..\info\ignored.txt -g bind26
+
+aladdin.py -bcrRt baidusd -s 15726 -g bind27
+aladdin.py -bcRt baidusd -g bind27
+aladdin.py -bcRt baidusd -p ..\info\95_aladin_id.txt -P ..\info\ignored.txt -g bind27
+
+aladdin.py -bcrRt baidusd -s 15726 -g bind28
+aladdin.py -bcRt baidusd -g bind28
+aladdin.py -bcRt baidusd -p ..\info\95_aladin_id.txt -P ..\info\ignored.txt -g bind28
+
+aladdin.py -bcrRt baidusd -s 15726 -g bind29
+aladdin.py -bcRt baidusd -g bind29
+aladdin.py -bcRt baidusd -p ..\info\95_aladin_id.txt -P ..\info\ignored.txt -g bind29
+
+aladdin.py -bcrRt baidusd -s 15726 -g bind30
+aladdin.py -bcRt baidusd -g bind30
+aladdin.py -bcRt baidusd -p ..\info\95_aladin_id.txt -P ..\info\ignored.txt -g bind30
+
+aladdin.py -bcrRt baidusd -s 15726 -g bind31
+aladdin.py -bcRt baidusd -g bind31
+aladdin.py -bcRt baidusd -p ..\info\95_aladin_id.txt -P ..\info\ignored.txt -g bind31
+
 goto loop
